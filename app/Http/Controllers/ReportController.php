6eeb2e00b7c852e2c8896e7820ad67d548a14b6e
si.haisyam <si.haisyam@gmail.com>
2025-05-20 18:16:42 +0700

    Pertemuan 13

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -22,21 +22,43 @@ class ReportController extends Controller
         }else if(request()->kasir_id){
             $transactions = RentalTransaction::where('kasir_id', request()->kasir_id)->get();
         } 
-        // $transactions = RentalTransaction::when(request()->search, function ($query) {
-        //     $query->where('name', 'like', '%' . request()->search . '%')
-        //           ->orWhere('phone', 'like', '%' . request()->search . '%');
-        // })->orderBy('created_at', 'desc')->paginate(10);
 
         return view('reports.index', compact('transactions', 'users'));
     }
 
     public function xls()
     {
+        $transactions = [];
+        $users = User::all();
 
+        if(request()->startDate && request()->endDate && request()->kasir_id){
+            $transactions = RentalTransaction::where('kasir_id', request()->kasir_id)->whereBetween('created_at', [request()->startDate, request()->endDate])
+            ->get();
+        }else if (request()->startDate && request()->endDate) {
+            $transactions = RentalTransaction::whereBetween('created_at', [request()->startDate, request()->endDate])
+            ->get();
+        }else if(request()->kasir_id){
+            $transactions = RentalTransaction::where('kasir_id', request()->kasir_id)->get();
+        } 
+        
+        return $transactions;
     }
 
     public function pdf()
     {
+        $transactions = [];
+        $users = User::all();
 
+        if(request()->startDate && request()->endDate && request()->kasir_id){
+            $transactions = RentalTransaction::where('kasir_id', request()->kasir_id)->whereBetween('created_at', [request()->startDate, request()->endDate])
+            ->get();
+        }else if (request()->startDate && request()->endDate) {
+            $transactions = RentalTransaction::whereBetween('created_at', [request()->startDate, request()->endDate])
+            ->get();
+        }else if(request()->kasir_id){
+            $transactions = RentalTransaction::where('kasir_id', request()->kasir_id)->get();
+        } 
+        
+        return $transactions;
     }
 }
